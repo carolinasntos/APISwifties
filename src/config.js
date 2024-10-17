60b6f837fff2dad67f8d741c3044efa0ff6b956c
carolinasntos <a01174992@tec.mx>
2024-10-17 00:16:49 -0600

    D

--- src/config.js
+++ src/config.js
@@ -7,6 +7,6 @@ export const PORT = process.env.PORT || 3000
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'cdlsr'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'sprmr4535'
-export const DB_NAME = process.env.DB_NAME || 'act9'
+export const DB_NAME = process.env.DB_NAME || 'NewSpot'
 export const DB_PORT = parseInt(process.env.DB_PORT,10) || 1433
 
